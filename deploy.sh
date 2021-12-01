dc2d2e0a74af7332ddb9d195037f6588bb0608c5
Raul La Rosa <rlarosa@scorbot.com>
2021-11-30 19:32:10 -0800

    add deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.raullarosa.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://raullarosa.github.io
+# git push -f git@github.com:raullarosa/raullarosa.github.io.git production
+
+# if you are deploying to https://raullarosa.github.io/raullarosa.com
+git push -f git@github.com:raullarosa/raullarosa.com.git origin/production
+
+cd -
